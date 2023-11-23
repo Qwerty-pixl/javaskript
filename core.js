f37bf5d1442fc258f7c8291c4355096566d727f0
Qwerty-pixl <gulanda727@gmail.com>
2023-11-23 20:24:28 +0600

    added new file js

---
+++ core.js
@@ -0,0 +1,49 @@
+// // let myName = ""
+
+// // myName = "Asan"
+// // myName = "Usen"
+// // console.log(myName)
+// // myName = "Michael"
+// // console.log(myName)
+// // myName = "mikle"
+// // console.log(myName)
+// // let n = 123;
+// // n = 12.345;
+
+// let myAge = 16 
+// let myHeight = 170 
+// let myIQ = 120
+// let myWidth = 52 
+// let myFootSize = 38
+// let myGrade = 11
+// console.log(myGrade)
+// // string
+// let myName = "Gulanda"
+// let colorOfMyFirstChildEyes = "brown"
+// let colorOfMyNewJacket = "Blue"
+
+
+// // boolean
+// document.getElementById("demo").innerHTML =
+// String(Boolean(0)) + "<br>" +
+// String(Boolean(1)) + "<br>" +
+// String([1,2,3,4]);
+// document.getElementById("demo").innerHTML = Boolean(10 > 9);
+
+
+// console.log("my name is " + myName + " i am " + myAge + " years old")
+
+// console.log(`my name is  ${myName} i am ${myAge} years old`)
+
+// let myName = prompt("Enter your name")
+// console.log(`Your name is ${myName}`)
+
+// let myAge = prompt("Enter your age")
+// console.log(`You are ${myAge} years old`)
+// if(myAge < 18){
+//     console.log('your age ' + myAge + " Ваш доступ ограничен!!!!")
+// }else{
+//     console.log('your age ' + myAge + " Ваш доступ открыт")
+// }
+
+
