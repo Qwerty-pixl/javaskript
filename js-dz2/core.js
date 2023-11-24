8ead16a3ff32190a9b60bf47981accc486abe74e
Qwerty-pixl <gulanda727@gmail.com>
2023-11-24 18:32:43 +0600

    added strings

---
+++ js-dz2/core.js
@@ -0,0 +1,9 @@
+const height = 5;
+const width = 8;
+
+const triangleArea = 0.5 * width * height;
+console.log('Площадь треугольника:' , triangleArea , 'кв.см')
+
+let hours = 56;
+const minutes = hours * 60;
+console.log('В 56 часах' , minutes , 'минут')
