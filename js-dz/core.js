09bc253ad30b83d0bc6dcaf55b4457faa5d971e8
Qwerty-pixl <gulanda727@gmail.com>
2023-11-24 15:36:30 +0600

    added summ

--- js-dz/core.js
+++ js-dz/core.js
@@ -1,89 +1,111 @@
-//strings
-let colorOfMilk = "white"
-let colorOfSnow = "white"
-let colorOfSky = "blue"
-let colorOfCloud = "white"
-let colorOfFlower = "pink"
-let colorOfTelegram = "blue"
-let colorOfSun = "sky"
-let colorOfLeaves = "green"
-let colorOfMyHair = "brown"
-let myName = "Gulanda"
-let myFirstName = "Tokonova"
-let myFavoriteLesson = "Math"
+// //strings
+// let colorOfMilk = "white"
+// let colorOfSnow = "white"
+// let colorOfSky = "blue"
+// let colorOfCloud = "white"
+// let colorOfFlower = "pink"
+// let colorOfTelegram = "blue"
+// let colorOfSun = "sky"
+// let colorOfLeaves = "green"
+// let colorOfMyHair = "brown"
+// let myName = "Gulanda"
+// let myFirstName = "Tokonova"
+// let myFavoriteLesson = "Math"
 
-console.log("Color Of Milk " + colorOfMilk + " color Of Snow " + colorOfSnow)
-console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
-console.log("Color Of Flower " + colorOfFlower + " Color Of Telegram " + colorOfTelegram)
-console.log("Color Of Sun " + colorOfSun + " Color Of Leaves " + colorOfLeaves)
-console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
-console.log("Color Of MyHair " + colorOfMyHair + " My Name is " + myName)
-console.log("My First Name " + myFirstName + " My Favorite Lesson is " + myFavoriteLesson)
-console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
-console.log("Color Of Flower " + colorOfFlower + " Color Of Telegram " + colorOfTelegram)
-//number
-let myAge = 16
-let myHeight = 170
-let myIQ = 120
-let myWidth = 52
-let myFootSize = 38
-let myGrade = 11
-let myMothersAge = 38
-let mySistersAge = 15
-let mySectionNum = 7
-
-
-console.log("my name is " + myName + " i am " + myAge + " years old")
-console.log("My Height is " + myHeight + " My IQ is " + myIQ)
-console.log(" My Width is " + myWidth + " My Foot Size is " + myFootSize)
-console.log("My Sisters Age is " + mySistersAge + " My Section Num is " + mySectionNum)
-
-// boolean
-
-let mynameisklara = false
-let iam20yearsold = false
-let mynameisGulanda = true
-let iam16yearsold = true
-let cloud = true
-let Sun = true
+// console.log("Color Of Milk " + colorOfMilk + " color Of Snow " + colorOfSnow)
+// console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
+// console.log("Color Of Flower " + colorOfFlower + " Color Of Telegram " + colorOfTelegram)
+// console.log("Color Of Sun " + colorOfSun + " Color Of Leaves " + colorOfLeaves)
+// console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
+// console.log("Color Of MyHair " + colorOfMyHair + " My Name is " + myName)
+// console.log("My First Name " + myFirstName + " My Favorite Lesson is " + myFavoriteLesson)
+// console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
+// console.log("Color Of Flower " + colorOfFlower + " Color Of Telegram " + colorOfTelegram)
+// //number
+// let myAge = 16
+// let myHeight = 170
+// let myIQ = 120
+// let myWidth = 52
+// let myFootSize = 38
+// let myGrade = 11
+// let myMothersAge = 38
+// let mySistersAge = 15
+// let mySectionNum = 7
 
 
+// console.log("my name is " + myName + " i am " + myAge + " years old")
+// console.log("My Height is " + myHeight + " My IQ is " + myIQ)
+// console.log(" My Width is " + myWidth + " My Foot Size is " + myFootSize)
+// console.log("My Sisters Age is " + mySistersAge + " My Section Num is " + mySectionNum)
 
-console.log(`my name is klara ${mynameisklara} i am 20 years old ${iam20yearsold}`)
-console.log(`my name is Gulanda ${mynameisGulanda} i am 16 years old ${iam16yearsold}`)
-console.log(`Today is ${cloud} Today is ${Sun}`)
+// // boolean
+
+// let mynameisklara = false
+// let iam20yearsold = false
+// let mynameisGulanda = true
+// let iam16yearsold = true
+// let cloud = true
+// let Sun = true
 
 
-// const truthyValue = Boolean(4 < 5)
-// const anotherTruthy = 4 < 5
 
-// console.log(truthyValue)
-// // true
-// console.log(anotherTruthy)
-// // true
+// console.log(`my name is klara ${mynameisklara} i am 20 years old ${iam20yearsold}`)
+// console.log(`my name is Gulanda ${mynameisGulanda} i am 16 years old ${iam16yearsold}`)
+// console.log(`Today is ${cloud} Today is ${Sun}`)
 
 
-// let age = 17;
-// let allowAccess = age >= 18;
+// // const truthyValue = Boolean(4 < 5)
+// // const anotherTruthy = 4 < 5
 
-let age = prompt("Пожалуйста, введите ваш возраст:");
+// // console.log(truthyValue)
+// // // true
+// // console.log(anotherTruthy)
+// // // true
 
-if (age >= 18) {
-    alert("Доступ разрешен!");
-} else {
-    alert("Извините, доступ запрещен для лиц младше 18 лет.");
-} 
 
-let isItWinterToday = "winter"
-if(isItWinterToday == "winter") {
-    console.log('Сейчас зима')
-} else{
-    console.log('Сейчас точно НЕ зима!')
-}
-// let place = prompt("Пожалуйста, введите ваше местоположение:");
+// // let age = 17;
+// // let allowAccess = age >= 18;
 
-// if (place >= Кыргызстан) {
-//     alert(" You are Kyrgyz");
+// let age = prompt("Пожалуйста, введите ваш возраст:");
+
+// if (age >= 18) {
+//     alert("Доступ разрешен!");
 // } else {
-//     alert("you are not kyrgyz");
+//     alert("Извините, доступ запрещен для лиц младше 18 лет.");
+// } 
+
+// let isItWinterToday = "winter"
+// if(isItWinterToday == "winter") {
+//     console.log('Сейчас зима')
+// } else{
+//     console.log('Сейчас точно НЕ зима!')
 // }
+// // let place = prompt("Пожалуйста, введите ваше местоположение:");
+
+// // if (place >= Кыргызстан) {
+// //     alert(" You are Kyrgyz");
+// // } else {
+// //     alert("you are not kyrgyz");
+// // }
+
+let summ = 5+5
+console.log(summ)
+
+let diff = 6754958-48549
+console.log(diff)
+
+let perimetrOfMuTable = 60+60+50+50
+console.log("perimetr Of My Table " + perimetrOfMuTable)
+
+let multiplication = 7859*866
+console.log(multiplication)
+
+let division = 9759/866
+console.log(division)
+
+
+
+let x = 10;
+let y = 20;
+let z = x + y;
+console.log(z);
