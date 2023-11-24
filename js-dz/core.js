dc768ec81d25195c74eaf951e90c82621a68d561
Qwerty-pixl <gulanda727@gmail.com>
2023-11-24 14:31:21 +0600

    added number

--- js-dz/core.js
+++ js-dz/core.js
@@ -1,12 +1,89 @@
+//strings
 let colorOfMilk = "white"
 let colorOfSnow = "white"
-let colorOfSky = "white"
+let colorOfSky = "blue"
 let colorOfCloud = "white"
-let colorOfFlower = "white"
-let colorOfTelegram = "white"
-let colorOfSun = "white"
-let colorOfLeaves = "white"
-let colorOf = "white"
+let colorOfFlower = "pink"
+let colorOfTelegram = "blue"
+let colorOfSun = "sky"
+let colorOfLeaves = "green"
+let colorOfMyHair = "brown"
 let myName = "Gulanda"
 let myFirstName = "Tokonova"
 let myFavoriteLesson = "Math"
+
+console.log("Color Of Milk " + colorOfMilk + " color Of Snow " + colorOfSnow)
+console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
+console.log("Color Of Flower " + colorOfFlower + " Color Of Telegram " + colorOfTelegram)
+console.log("Color Of Sun " + colorOfSun + " Color Of Leaves " + colorOfLeaves)
+console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
+console.log("Color Of MyHair " + colorOfMyHair + " My Name is " + myName)
+console.log("My First Name " + myFirstName + " My Favorite Lesson is " + myFavoriteLesson)
+console.log("Color Of Sky " + colorOfMilk + " Color Of Cloud " + colorOfCloud)
+console.log("Color Of Flower " + colorOfFlower + " Color Of Telegram " + colorOfTelegram)
+//number
+let myAge = 16
+let myHeight = 170
+let myIQ = 120
+let myWidth = 52
+let myFootSize = 38
+let myGrade = 11
+let myMothersAge = 38
+let mySistersAge = 15
+let mySectionNum = 7
+
+
+console.log("my name is " + myName + " i am " + myAge + " years old")
+console.log("My Height is " + myHeight + " My IQ is " + myIQ)
+console.log(" My Width is " + myWidth + " My Foot Size is " + myFootSize)
+console.log("My Sisters Age is " + mySistersAge + " My Section Num is " + mySectionNum)
+
+// boolean
+
+let mynameisklara = false
+let iam20yearsold = false
+let mynameisGulanda = true
+let iam16yearsold = true
+let cloud = true
+let Sun = true
+
+
+
+console.log(`my name is klara ${mynameisklara} i am 20 years old ${iam20yearsold}`)
+console.log(`my name is Gulanda ${mynameisGulanda} i am 16 years old ${iam16yearsold}`)
+console.log(`Today is ${cloud} Today is ${Sun}`)
+
+
+// const truthyValue = Boolean(4 < 5)
+// const anotherTruthy = 4 < 5
+
+// console.log(truthyValue)
+// // true
+// console.log(anotherTruthy)
+// // true
+
+
+// let age = 17;
+// let allowAccess = age >= 18;
+
+let age = prompt("Пожалуйста, введите ваш возраст:");
+
+if (age >= 18) {
+    alert("Доступ разрешен!");
+} else {
+    alert("Извините, доступ запрещен для лиц младше 18 лет.");
+} 
+
+let isItWinterToday = "winter"
+if(isItWinterToday == "winter") {
+    console.log('Сейчас зима')
+} else{
+    console.log('Сейчас точно НЕ зима!')
+}
+// let place = prompt("Пожалуйста, введите ваше местоположение:");
+
+// if (place >= Кыргызстан) {
+//     alert(" You are Kyrgyz");
+// } else {
+//     alert("you are not kyrgyz");
+// }
