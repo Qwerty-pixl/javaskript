c4e426377391b4729728fdea4170180e93e0f0c8
Qwerty-pixl <gulanda727@gmail.com>
2023-11-27 14:29:34 +0600

    added new core.js

---
+++ js-3/core.js
@@ -0,0 +1,22 @@
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+
+// if (year < 2015) {
+//   alert( 'Это слишком рано...' );
+// } else if (year > 2015) {
+//   alert( 'Это поздновато' );
+// } else {
+//   alert( 'Верно!' );
+// }
+let temp = 10
+
+if(temp > 29){
+    console.log('Жарко!!!')
+}else if(temp > 24){
+    console.log('Классно!!!')
+}else if(temp > 15){
+    console.log('Нормально!!!')
+}else if(temp > 0){
+    console.log('Холодно!!!')
+}else{
+    console.log('Брр меньше нуля!!! меньше нуля')
+}
